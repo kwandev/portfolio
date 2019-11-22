b356ac6583ef062ff0a351a682a2df8f971818c9
lkh <lkh07816@gmail.com>
2019-11-22 17:52:26 +0900

    header 내용 정리

--- components/Headers.vue
+++ components/Headers.vue
@@ -8,7 +8,7 @@
               About
             </h4>
             <p class="text-muted">
-              Add some information about the album below, the author, or any other background context. Make it a few sentences long so folks can pick up some informative tidbits. Then, link them off to some social networking sites or contact information.
+              안녕하세요. 프론트엔드 개발자 이경환입니다.<br>해당 페이지는 세부 경력기술을 위해 만들었습니다.<br>연락은 이메일을 이용해주세요.
             </p>
           </div>
           <div class="col-sm-4 offset-md-1 py-4">
@@ -16,9 +16,9 @@
               Contact
             </h4>
             <ul class="list-unstyled">
-              <li><a href="#" class="text-white">Follow on Twitter</a></li>
-              <li><a href="#" class="text-white">Like on Facebook</a></li>
-              <li><a href="#" class="text-white">Email me</a></li>
+              <li><a href="mailto:rudghks7816@gmail.com" class="text-white">rudghks7816@gmail.com</a></li>
+              <li><a href="https://leekyeonghwan.github.io" class="text-white" target="_blank">Blog</a></li>
+              <li><a href="https://github.com/LeeKyeongHwan" class="text-white" target="_blank">Github</a></li>
             </ul>
           </div>
         </div>
@@ -27,21 +27,7 @@
     <b-navbar type="dark" variant="dark" toggleable fixed="top" class="shadow-sm">
       <div class="container d-flex justify-content-between">
         <b-navbar-brand to="/">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="20"
-            height="20"
-            fill="none"
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            aria-hidden="true"
-            class="mr-2"
-            viewBox="0 0 24 24"
-            focusable="false"
-          ><path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z" /><circle cx="12" cy="13" r="4" /></svg>
-          <strong>Album</strong>
+          <strong>PORTFOLIO</strong>
         </b-navbar-brand>
         <b-navbar-toggle target="navbarHeader">
           <span class="navbar-toggler-icon" />
